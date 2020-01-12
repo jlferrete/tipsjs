c814bbd0c4d507538e78bfc897f829a84a03ce01
JlFerrete <joseluisferrete@gmail.com>
2020-01-12 13:11:15 +0100

    Tweak and Adjust output

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const output = document.querySelector(".output");
 console.log(button);
 button.addEventListener("click", function() {
     const cost = document.querySelector("input");
-    console.log(cost.value);
-    output.innerHTML = cost.value;
+    console.log(cost.value * 0.15);
+    let tip = (cost.value * 0.15).toFixed(2);
+    let temp = '<h1>You should tip $' + tip + ' on $' + cost.value + '</h1>';
+    output.innerHTML = temp;
 })
