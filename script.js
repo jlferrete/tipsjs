20d69df82b0a23b37532d2a7d341f83b2e428b31
JlFerrete <joseluisferrete@gmail.com>
2020-01-12 13:14:27 +0100

    Backtick template JavaScript

--- script.js
+++ script.js
@@ -7,6 +7,6 @@ button.addEventListener("click", function() {
     const cost = document.querySelector("input");
     console.log(cost.value * 0.15);
     let tip = (cost.value * 0.15).toFixed(2);
-    let temp = '<h1>You should tip $' + tip + ' on $' + cost.value + '</h1>';
+    let temp = `<h1>You should tip $${tip} on $${cost.value}</h1>`;
     output.innerHTML = temp;
 })
